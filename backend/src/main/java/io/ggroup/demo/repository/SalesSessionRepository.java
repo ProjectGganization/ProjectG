0eed67cac2353b05b8daa0b027f8e623fb43852e
ng4nt <ngantran2133@gmail.com>
2026-03-27 00:23:01 +0200

    removed SalesSession entity and model

--- backend/src/main/java/io/ggroup/demo/repository/SalesSessionRepository.java
+++
@@ -1,7 +0,0 @@
-package io.ggroup.demo.repository;
-
-import io.ggroup.demo.model.SalesSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesSessionRepository extends JpaRepository<SalesSession, Integer> {
-}
