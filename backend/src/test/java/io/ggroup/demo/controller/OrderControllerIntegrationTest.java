2df49b62ba0ce40a893d2c5f6ee92e19a9491875
PhongNgvyen <bhp978@myy.haaga-helia.fi>
2026-04-10 15:43:00 +0300

    Intergation test branch

---
+++ backend/src/test/java/io/ggroup/demo/controller/OrderControllerIntegrationTest.java
@@ -0,0 +1,60 @@
+package io.ggroup.demo.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import io.ggroup.demo.model.Order;
+import io.ggroup.demo.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class OrderControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    @DisplayName("Test if admin can create new order")
+    @WithMockUser(username = "admin@test.com", roles = "ADMIN")
+    void shouldCreateNewOrder() throws Exception {
+
+        long ordersBefore = orderRepository.count();
+
+        String orderJson = """
+                {
+                  "customer": {
+                    "customerId": 1
+                  },
+                  "date": "2026-01-10T17:32:24.719",
+                  "isRefunded": false,
+                  "isPaid": true
+                }
+                """;
+
+        mockMvc.perform(post("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderJson))
+                .andExpect(status().isOk());
+
+        long ordersAfter = orderRepository.count();
+
+        assertThat(ordersAfter).isEqualTo(ordersBefore + 1);
+
+        Order savedOrder = orderRepository.findAll().get((int) ordersAfter - 1);
+        assertThat(savedOrder).isNotNull();
+    }
+}
